d8630249b30bdd37400a5e8c73568be8cc8fbd30
DVDzig <102020058+DVDzig@users.noreply.github.com>
2025-04-01 20:17:24 +0300

    Обновляем оплату Юкассы

--- register_webhook.py
+++ register_webhook.py
@@ -5,27 +5,25 @@ import uuid
 # === Данные YooKassa ===
 shop_id = "1059797"
 secret_key = "live_oRsbsdcpTtq0lIPiq4X_JddZYvMdGii7-VCwT4NsMIY"
-
-# === Твой Render-домен
 webhook_url = "https://tg-bot-ai-teyr.onrender.com/payment/result"
 
-# === Авторизация ===
-auth_token = base64.b64encode(f"{shop_id}:{secret_key}".encode()).decode()
+# === Basic Auth
+auth_string = f"{shop_id}:{secret_key}"
+auth_bytes = auth_string.encode("utf-8")
+auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+
 headers = {
-    "Authorization": f"Basic {auth_token}",
+    "Authorization": f"Basic {auth_base64}",
     "Content-Type": "application/json",
-    "Idempotence-Key": str(uuid.uuid4()),
+    "Idempotence-Key": str(uuid.uuid4())
 }
 
-# === Тело запроса ===
 data = {
     "event": "payment.succeeded",
     "url": webhook_url
 }
 
-# === Отправка запроса ===
 response = requests.post("https://api.yookassa.ru/v3/webhooks", headers=headers, json=data)
 
-# === Ответ сервера ===
 print("Status Code:", response.status_code)
 print("Response:", response.text)
