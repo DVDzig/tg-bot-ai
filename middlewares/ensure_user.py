e6ab167552cfc6b812f8d8e7970470be76b448a6
DVDzig <102020058+DVDzig@users.noreply.github.com>
2025-04-16 22:34:08 +0300

    Улучшение дизайна. Сброс пользователя до нуля после отсутствия

--- middlewares/ensure_user.py
+++ middlewares/ensure_user.py
@@ -1,7 +1,11 @@
 from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery
 from typing import Callable, Dict, Any, Awaitable, Union
-from services.user_service import get_or_create_user
+from services.user_service import get_or_create_user, get_user_row_by_id
+from aiogram.fsm.context import FSMContext
+from datetime import datetime, timedelta
+import pytz
+from keyboards.common import get_main_keyboard
 
 class EnsureUserMiddleware(BaseMiddleware):
     async def __call__(
@@ -14,4 +18,22 @@ class EnsureUserMiddleware(BaseMiddleware):
         if user:
             await get_or_create_user(user)
             data["event_from_user"] = user  # ✅ добавляем в data
+
+            row = await get_user_row_by_id(user.id)
+            if row:
+                last_str = row.get("last_interaction")
+                if last_str:
+                    try:
+                        last_time = datetime.strptime(last_str, "%Y-%m-%d %H:%M:%S").replace(tzinfo=pytz.timezone("Europe/Moscow"))
+                        now = datetime.now(pytz.timezone("Europe/Moscow"))
+
+                        if now - last_time > timedelta(minutes=15):
+                            state: FSMContext = data.get("state")
+                            if state:
+                                await state.clear()
+                            await event.answer("👋 Рады видеть тебя снова! Вернёмся в главное меню:")
+                            await event.answer("Выбери действие:", reply_markup=get_main_keyboard())
+                    except Exception as e:
+                        print(f"[Middleware TimeParse Error] {e}")
+
         return await handler(event, data)
