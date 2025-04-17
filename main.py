46f0438e225deb1f3be899b99cb3bddc6dc023e6
DVDzig <102020058+DVDzig@users.noreply.github.com>
2025-04-17 12:29:24 +0300

    Улучшение дизайна. Добавление фото от пользователя 03

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ from handlers import (
     shop_handler,
     leaderboard_handler,
     program_handler,    
-    shop_navigation,
-    photo_handler
+    shop_navigation
 )
 from middlewares.ensure_user import EnsureUserMiddleware
 from utils.scheduler import schedule_all_jobs, schedule_monthly_bonus
@@ -45,7 +44,6 @@ dp.include_router(leaderboard_handler.router)
 dp.include_router(program_handler.router)
 dp.include_router(start_handler.router)
 dp.include_router(shop_navigation.router)
-dp.include_router(photo_handler.router)
 
 
 # === Telegram Bot & Dispatcher ===
