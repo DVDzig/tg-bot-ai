96db0791b653a7a26f31143661e650de28927cae
DVDzig <102020058+DVDzig@users.noreply.github.com>
2025-04-19 22:13:20 +0300

    Оценке ответов через эмодзи 07

--- handlers/rating_handler.py
+++ handlers/rating_handler.py
@@ -2,11 +2,13 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from services.google_sheets_service import log_user_rating
 from services.user_service import get_user_row_by_id
+from aiogram.fsm.context import FSMContext
 
 router = Router()
 
 @router.callback_query(F.data.startswith("rating:"))
-async def handle_rating(callback: CallbackQuery):
+async def handle_rating(callback: CallbackQuery, state: FSMContext):
+    print(f"[DEBUG] Callback data: {callback.data}")
     rating = callback.data.split(":")[1]
     user = callback.from_user
 
