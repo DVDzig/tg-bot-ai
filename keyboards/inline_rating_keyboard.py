3c23a092a0cb9c7441207c2c7d607ca33202aa78
DVDzig <102020058+DVDzig@users.noreply.github.com>
2025-04-19 22:45:03 +0300

    Оценке ответов через эмодзи 11

--- keyboards/inline_rating_keyboard.py
+++ keyboards/inline_rating_keyboard.py
@@ -2,8 +2,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_rating_keyboard() -> InlineKeyboardMarkup:
     emojis = ["🤩", "😊", "😐", "🙁", "😡"]
-    buttons = [
-        [InlineKeyboardButton(text=emoji, callback_data=f"rating:{emoji}")]
-        for emoji in emojis
-    ]
-    return InlineKeyboardMarkup(inline_keyboard=buttons)
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text=emoji, callback_data=f"rating:{emoji}") for emoji in emojis]
+        ]
+    )
